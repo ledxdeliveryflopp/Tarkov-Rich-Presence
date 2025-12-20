920ef75d5c90286162f79082ef09c86e738012c9
LedxDelivery <korstim18@gmail.com>
2025-12-21 04:15:52 +0700

    1) обновил токен доступа к репозиторию

--- src/github/api.py
+++ src/github/api.py
@@ -10,7 +10,7 @@ class GitHubApi:
     def __init__(self):
         super().__init__()
         self.latest_release = f'{const.git.GITHUB_API}{const.git.GITHUB_OWNER}/{const.git.GITHUB_REPO}/releases/latest'
-        self.token = 'Bearer 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' # noqa
+        self.token = 'Bearer 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' # noqa
 
     @logger.catch
     def get_latest_release_version(self) -> str:
